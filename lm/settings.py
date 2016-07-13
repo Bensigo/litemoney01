22d54429328a78c644bff7d9fa442d625b51df17
larry sigo <sigocode@gmail.com>
2016-07-13 19:21:27 +0100

    update

--- lm/settings.py
+++ lm/settings.py
@@ -122,12 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS =(os.path.join(BASE_DIR,'static'),)
-# STATIC_ROOT = os.path.join(BASE_DIR,'static-cdn')
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-# MEDIA_ROOT = os.path.join(BASE_DIR,'media-cdn')
-# MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS =(os.path.join(BASE_DIR,'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR,'static-cdn')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media-cdn')
+MEDIA_URL = '/media/'
 
 #settings for emails
 EMAIL_BACKEND = 'django.core.mail.backends.console.email'

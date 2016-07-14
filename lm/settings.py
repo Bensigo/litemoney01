46aed2113e63f52bcf5a89855d1e0a90ed325954
larry sigo <sigocode@gmail.com>
2016-07-14 02:57:25 +0100

    upgrade 1.1 and added 404,500 page

--- lm/settings.py
+++ lm/settings.py
@@ -132,7 +132,7 @@ MEDIA_URL = '/media/'
 #settings for emails
 EMAIL_BACKEND = 'django.core.mail.backends.console.email'
 EMAIL_HOST ='smtp.gmail.com'
-EMAIL_HOST_PASSWORD ='Fa3<>TJY'
+EMAIL_HOST_PASSWORD ='84A8E46E3C'
 EMAIL_HOST_USER ='info@litemoney.net'
 EMAIL_USE_TLS = 1
 EMAIL_PORT = 587

ebbd83156a932709fa5b7f8ce36e0faf6275aee0
larry sigo <sigocode@gmail.com>
2016-07-16 12:59:53 +0100

    adding pagination to news list

--- News/views.py
+++ News/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render,get_object_or_404
 from .models import  News
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
 def news_list(request):
 	#diplay all the news in the page
 	template = 'newsTemplates/index.html'
-	news = News.objects.all().order_by('-pk')
+	news_list = News.objects.all().order_by('-pk')
+	paginator = Paginator(contact_list, 10) # Show 10 news per page
+	page = request.GET.get('page')
+	try:
+        news = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        news = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        news = paginator.page(paginator.num_pages)
+
 	context = {'news':news}
 	return render (request,template,context)
 

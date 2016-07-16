00024c56f70444755c9390a618c972e74c425583
larry sigo <sigocode@gmail.com>
2016-07-16 11:19:09 +0100

    work on the contact form

--- lm/settings.py
+++ lm/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'django.contrib.sites',
     'News',
     'lmplatform',
     #third party app
     'contact_form',
 
 ]
+SITE_ID = 1
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',

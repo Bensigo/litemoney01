0c7aa44d309c90981a3ef98fe34711b23f91a5ea
larry sigo <sigocode@gmail.com>
2016-07-16 11:27:59 +0100

    work on the contact form

--- lm/urls.py
+++ lm/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
                url(r'^FQAS/$','staticPages.views.FQAS',name ='FQAS'),
                url(r'^about/$','staticPages.views.about',name ='about'),
                url(r'^pricing/$','staticPages.views.pricing',name ='pricing'),
-               url(r'^contact/', include('contact_form.urls')),
+               url(r'^contact/', include('contact_form.urls'), name ='contact'),
                ]
 handler404 = 'News.views.error404'
 handler500 = 'News.views.error500'
